566e6d1e2af54ed34203684d727c4274519963e7
Joe Granville <874605+jwgranville@users.noreply.github.com>
2026-01-18 16:51:54 -0800

    Preflight: create branch for preflight script

---
+++ preflight.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+A preflight/sanity check script for subiquity/cloud-init installations.
+"""
+
+__author__ = "Joe Granville"
+__date__ = "20260118"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__email__ = "jwgranville@gmail.com"
+__status__ = "Proof-of-concept"
+
---
+++ preflight_checks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Installer-specific checks for preflight.py.
+"""
+
+__author__ = "Joe Granville"
+__date__ = "20260118"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__email__ = "jwgranville@gmail.com"
+__status__ = "Proof-of-concept"
+
